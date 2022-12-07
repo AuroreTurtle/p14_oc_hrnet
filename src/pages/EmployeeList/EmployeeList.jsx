9926eb3b08bcba9c88f6a94917ffbae09dcddf07
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2022-12-07 11:07:08 +0100

    Add EmployeeList page

--- src/pages/EmployeeList/EmployeeList.jsx
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -1,3 +1,14 @@
-function EmployeeList() {}
+import "./EmployeeList.css";
+
+function EmployeeList() {
+    return (
+        <main>
+            <div id="employee-div" className="container container-table">
+                <h2>Current Employees</h2>
+                <table id="employee-table" className="display"></table>
+            </div>
+        </main>
+    );
+}
 
 export default EmployeeList;
