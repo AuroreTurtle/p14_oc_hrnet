5d5e20c92517ee594a99fd523b16c38c89b5ca11
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2022-12-24 15:31:32 +0100

    Add the table of employees

--- src/pages/EmployeeList/EmployeeList.jsx
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -1,11 +1,57 @@
+import React, { useMemo } from "react";
+import { useSelector } from "react-redux";
+import MaterialReactTable from "material-react-table";
 import "./EmployeeList.css";
 
 function EmployeeList() {
+    const employees = useSelector((state) => state.employeeList.employee);
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: "firstName",
+                header: "First Name",
+            },
+            {
+                accessorKey: "lastName",
+                header: "Last Name",
+            },
+            {
+                accessorKey: "startDate",
+                header: "Start Date",
+            },
+            {
+                accessorKey: "department",
+                header: "Department",
+            },
+            {
+                accessorKey: "dateOfBirth",
+                header: "Date of Birth",
+            },
+            {
+                accessorKey: "street",
+                header: "Street",
+            },
+            {
+                accessorKey: "city",
+                header: "City",
+            },
+            {
+                accessorKey: "state",
+                header: "State",
+            },
+            {
+                accessorKey: "zipCode",
+                header: "Zip Code",
+            },
+        ],
+        []
+    );
+
     return (
         <main>
             <div id="employee-div" className="container container-table">
                 <h2>Current Employees</h2>
-                <table id="employee-table" className="display"></table>
+                <MaterialReactTable columns={columns} data={employees} />
             </div>
         </main>
     );
